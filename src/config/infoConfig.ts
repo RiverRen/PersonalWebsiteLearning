4ebb32bedced5073c981048e2f136f46143af89c
RiverRen <i_pride@126.com>
2025-05-25 18:58:57 +0800

    mini changes tried

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,12 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'RiverRen Tsao'
+export const headline = 'Undergraduate in PKU majoring in Psychology & Cognitive Sciences'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+  "Hi! My name is RiverRen Tsao. I'm from Tianjin."
+export const email = 'riverren@stu.pku.edu.cn'
+export const githubUsername = 'RiverRen'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
